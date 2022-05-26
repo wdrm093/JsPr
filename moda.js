415b8c77b3865e1431994859a80c812fc67b4f50
wdrm093 <ccp191293@gmail.com>
2022-05-26 13:24:06 -0400

    orden de code

--- moda.js
+++ moda.js
@@ -6,7 +6,7 @@ const lista1=[
             lista1Count[elemento]+=1;
         }else{lista1Count[elemento]=1;
         }});
-        const lista1Array=Object.entries(lista1Count).sort(function(elementoA,elementoB)
-        {return elementoA[1]-elementoB[1];
+        const lista1Array=Object.entries(lista1Count).sort(function(elementoA,elementoB){
+            return elementoA[1]-elementoB[1];
         });
         const moda=lista1Array[lista1Array.length-1];
