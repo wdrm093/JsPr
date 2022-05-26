46c0a1f8836c462f3f4f20b877285ebd0eacb1a8
wdrm093 <ccp191293@gmail.com>
2022-05-26 13:46:10 -0400

    agregando promedio ponderado

--- moda.js
+++ moda.js
@@ -4,7 +4,8 @@ const lista1=[
     lista1.map(function(elemento){
         if(lista1Count[elemento]){
             lista1Count[elemento]+=1;
-        }else{lista1Count[elemento]=1;
+        }else{
+            lista1Count[elemento]=1;
         }});
         const lista1Array=Object.entries(lista1Count).sort(function(elementoA,elementoB){
             return elementoA[1]-elementoB[1];
