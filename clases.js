0f35ae7a64e8c3dfa05a61d8c6ae9389c9331f3e
wdrm093 <ccp191293@gmail.com>
2022-06-07 10:16:41 -0400

    herencias

--- clases.js
+++ clases.js
@@ -10,17 +10,17 @@ const amor ={
         this.caracteristicas.push(nueva);
     },
 };
-//prototipos con instancia
-function Student(name, age, cursosAprobados) {
+//prototipos (clases) con instancia
+function Student1(name, age, cursosAprobados) {
     this.name = name;
     this.age = age;
     this.cursosAprobados = cursosAprobados;
   }
-  Student.prototype.aprobarCurso = function (nuevoCursito) {
+  Student1.prototype.aprobarCurso = function (nuevoCursito) {
     this.cursosAprobados.push(nuevoCursito);
   }
   
-  const brissa = new Student(
+  const brissa = new Student1(
     "Brissa Jhulianne",
     21,
     [
@@ -29,120 +29,153 @@ function Student(name, age, cursosAprobados) {
     ],
   );
 
-  class Student2{
-    constructor({
-      name,
-      age,
-      cursosAprobados,
-      email
-    }){
-      this.name = name;
-      this.age = age;
-      this.email = email;
-      this.cursosAprobados = cursosAprobados;
-    }
-    aprobarCurso(nuevoCursito){
-      this.cursosAprobados.push(nuevoCursito);
+class Course {
+  constructor({
+    name,
+    classes = [],
+    isFree = false,
+    lang = "spanish",
+  }) {
+    this._name = name;
+    this.classes = classes;
+    this.isFree = isFree;
+    this.lang = lang;
+  }
+
+  get name() {
+    return this._name;
+  }
+
+  set name(nuevoNombrecito) {
+    if (nuevoNombrecito === "Curso Malito de Programación Básica") {
+      console.error("Web... no");
+    } else {
+      this._name = nuevoNombrecito; 
     }
   }
-  const waltercito = new Student2({
-    name:"Waltercito",
-    age:29,
-    cursosAprobados: ["Curso del POO", "Curso Practico POO"]
-  }  
-  )
-  class LearningPath {
-    constructor({
-      name,
-      courses = [],
-    }) {
-      this.name = name;
-      this.courses = courses;
+}
+const cursoProgBasica = new Course({
+  name: "Curso Gratis de Programación Básica",
+  isFree: true,
+});
+const cursoDefinitivoHTML = new Course({
+  name: "Curso Definitivo de HTML y CSS",
+});
+const cursoPracticoHTML = new Course({
+  name: "Curso Practico de HTML y CSS",
+  lang: "english",
+});
+
+class LearningPath {
+  constructor({
+    name,
+    courses = [],
+  }) {
+    this.name = name;
+    this.courses = courses;
+  }
+}
+const escuelaWeb = new LearningPath({
+  name: "Escuela de Desarrollo Web",
+  courses: [
+    cursoProgBasica,
+    cursoDefinitivoHTML,
+    cursoPracticoHTML,
+  ],
+});
+const escuelaData = new LearningPath({
+  name: "Escuela de Data Science",
+  courses: [
+    cursoProgBasica,
+    "Curso DataBusiness",
+    "Curso Dataviz",
+  ],
+});
+const escuelaVgs = new LearningPath({
+  name: "Escuela de Vidweojuegos",
+  courses: [
+    cursoProgBasica,
+    "Curso de Unity",
+    "Curso de Unreal",
+  ],
+})
+
+class Student {
+  constructor({
+    name,
+    email,
+    username,
+    twitter = undefined,
+    instagram = undefined,
+    facebook = undefined,
+    approvedCourses = [],
+    learningPaths = [],
+  }) {
+    this.name = name;
+    this.email = email;
+    this.username = username;
+    this.socialMedia = {
+      twitter,
+      instagram,
+      facebook,
+    };
+    this.approvedCourses = approvedCourses;
+    this.learningPaths = learningPaths;
+  }
+}
+
+class FreeStudent extends Student {
+  constructor(props) {
+    super(props);
+  }
+  approveCourse(newCourse) {
+    if (newCourse.isFree) {
+      this.approvedCourses.push(newCourse);
+    } else {
+      console.warn("Lo sentimos, " + this.name + ", solo puedes tomar cursos abiertos");
     }
   }
-  
-  const escuelaWeb = new LearningPath({
-    name: "Escuela de Desarrollo Web",
-    courses: [
-      cursoProgBasica,
-      cursoDefinitivoHTML,
-      cursoPracticoHTML,
-    ],
-  });
-  
-  const escuelaData = new LearningPath({
-    name: "Escuela de Data Science",
-    courses: [
-      cursoProgBasica,
-      "Curso DataBusiness",
-      "Curso Dataviz",
-    ],
-  });
-  
-  const escuelaVgs = new LearningPath({
-    name: "Escuela de Vidweojuegos",
-    courses: [
-      cursoProgBasica,
-      "Curso de Unity",
-      "Curso de Unreal",
-    ],
-  })
-  
-  class Student {
-    constructor({
-      name,
-      email,
-      username,
-      twitter = undefined,
-      instagram = undefined,
-      facebook = undefined,
-      approvedCourses = [],
-      learningPaths = [],
-    }) {
-      this.name = name;
-      this.email = email;
-      this.username = username;
-      this.socialMedia = {
-        twitter,
-        instagram,
-        facebook,
-      };
-      this.approvedCourses = approvedCourses;
-      this.learningPaths = learningPaths;
+}
+
+class BasicStudent extends Student {
+  constructor(props) {
+    super(props);
+  }
+  approveCourse(newCourse) {
+    if (newCourse.lang !== "english") {
+      this.approvedCourses.push(newCourse);
+    } else {
+      console.warn("Lo sentimos, " + this.name + ", no puedes tomar cursos en inglés");
     }
   }
-  
-  const juan2 = new Student({
-    name: "JuanDC",
-    username: "juandc",
-    email: "juanito@juanito.com",
-    twitter: "fjuandc"
-  });
-  
-  const miguelito2 = new Student({
-    name: "Miguelito",
-    username: "migelitofeliz",
-    email: "miguelito@juanito.com",
-    instagram: "migelito_feliz"
-  });
-  class Course {
-    constructor({
-      name,
-      classes = [],
-    }) {
-      this._name = name;
-      this.classes = classes;
-    }
-  
-    get name() {
-      return this._name;
-    }
-  
-    set name(nuevoNombrecito) {
-      if (nuevoNombrecito === "Curso Malito de Programación Básica") {
-        console.error("Web... no");
-      } else {
-        this._name = nuevoNombrecito; 
-      }
-    }
+}
+
+class ExpertStudent extends Student {
+  constructor(props) {
+    super(props);
   }
+  approveCourse(newCourse) {
+    this.approvedCourses.push(newCourse);
+  }
+}
+const juan = new FreeStudent({
+  name: "JuanDC",
+  username: "juandc",
+  email: "juanito@juanito.com",
+  twitter: "fjuandc",
+  learningPaths: [
+    escuelaWeb,
+    escuelaVgs,
+  ],
+});
+
+const miguelito = new BasicStudent({
+  name: "Miguelito",
+  username: "migelitofeliz",
+  email: "miguelito@juanito.com",
+  instagram: "migelito_feliz",
+  learningPaths: [
+    escuelaWeb,
+    escuelaData,
+  ],
+});
